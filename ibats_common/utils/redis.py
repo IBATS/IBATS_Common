f47f25cc4d796a0e8a15b394ef8096be279a902b
mmmaaaggg <mmmaaaggg@163.com>
2018-09-27 11:45:34 +0800

    First push

---
+++ ibats_common/utils/redis.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/6/16 17:54
+@File    : redis.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+from redis import StrictRedis, ConnectionPool
+from redis.client import PubSub
+from ibats_common.common import PeriodType
+
+_redis_client_dic = {}
+
+
+def get_channel(market=None, period: PeriodType=PeriodType.Min1, symbol=''):
+    """
+    'md.{market}.{period}.{symbol}' or 'md.{period}.{symbol}'
+    例如：
+    md.huobi.Min1.ethusdt
+    md.huobi.Tick.eosusdt
+    通过 redis-cli 可以 PUBSUB CHANNELS 查阅活跃的频道
+    :param market:
+    :param period:
+    :param symbol:
+    :return:
+    """
+    if market:
+        channel_str = f'md.{market}.{period.name}.{symbol}'
+    else:
+        channel_str = f'md.{period.name}.{symbol}'
+    #     md.market.tick.pair
+    return channel_str
+
+
+def get_redis(host, port, db=0) -> StrictRedis:
+    """
+    get StrictRedis object
+    :param db:
+    :return:
+    """
+    if db in _redis_client_dic:
+        redis_client = _redis_client_dic[db]
+    else:
+        conn = ConnectionPool(host=host, port=port, db=db)
+        redis_client = StrictRedis(connection_pool=conn)
+        _redis_client_dic[db] = redis_client
+    return redis_client
---
+++ setup.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/6/14 16:07
+@File    : setup.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as rm:
+    long_description = rm.read()
+
+setup(name='IBATSCommon',
+      version='0.1.0',
+      description='IBATS（Integration Backtest Analysis Trade System）的公共模块，所有Feeder, Trader均集成这个模块，并使用其提供的一些公共工具',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='MG',
+      author_email='mmmaaaggg@163.com',
+      url='https://github.com/IBATS/IBATSCommon',
+      packages=find_packages(),
+      python_requires='>=3.6',
+      classifiers=(
+          "Programming Language :: Python :: 3 :: Only",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: Unix",
+          "Operating System :: POSIX",
+          "License :: OSI Approved :: MIT License",
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: No Input/Output (Daemon)",
+          "Intended Audience :: Developers",
+          "Natural Language :: Chinese (Simplified)",
+          "Topic :: Software Development",
+      ),
+      install_requires=[
+          'pandas>=0.23.0',
+          'redis>=2.10.6',
+          'SQLAlchemy>=1.2.8',
+      ])
