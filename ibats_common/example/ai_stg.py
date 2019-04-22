3e6cc32ff6c2027b7fe224b57b5f433712a5a97d
mmmaaaggg <mmmaaaggg@163.com>
2019-04-22 17:55:51 +0800

    bug fix

--- ibats_common/example/ai_stg.py
+++ ibats_common/example/ai_stg.py
@@ -12,10 +12,9 @@ import os
 from ibats_utils.mess import get_last_idx, get_folder_path
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import random
-from ibats_common.common import PeriodType, RunMode, BacktestTradeMode, ExchangeName, ContextKey, Direction
+from ibats_common.analysis.plot import show_rr_with_md
+from ibats_common.common import PeriodType, RunMode, BacktestTradeMode, ExchangeName, ContextKey, Direction, CalcMode
 from ibats_common.strategy import StgBase
 from ibats_common.strategy_handler import strategy_handler_factory
 from ibats_local_trader.agent.td_agent import *
@@ -459,6 +458,7 @@ def _test_use(is_plot):
         trade_agent_params = {
             'trade_mode': BacktestTradeMode.Order_2_Deal,
             'init_cash': 1000000,
+            "calc_mode": CalcMode.Margin,
         }
         strategy_handler_param = {
             'date_from': '2010-1-1',  # 策略回测历史数据，回测指定时间段的历史行情
@@ -485,6 +485,7 @@ def _test_use(is_plot):
         from ibats_common.analysis.plot import show_order, show_cash_and_margin
         show_order(stg_run_id, module_name_replacement_if_main='ibats_common.example.ai_stg')
         show_cash_and_margin(stg_run_id)
+        show_rr_with_md(stg_run_id, module_name_replacement_if_main='ibats_common.example.ai_stg')
 
     return stg_run_id
 
