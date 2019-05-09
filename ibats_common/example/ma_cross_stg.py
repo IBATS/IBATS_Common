bacc3359c8360b3d794c6e054c4c3d83164aa558
mmmaaaggg <mmmaaaggg@163.com>
2019-05-09 16:48:30 +0800

    plot --> plot_db

--- ibats_common/example/ma_cross_stg.py
+++ ibats_common/example/ma_cross_stg.py
@@ -9,7 +9,6 @@
 """
 import time
 import logging
-from ibats_common.analysis.plot import show_rr_with_md
 from ibats_common.common import PeriodType, RunMode, BacktestTradeMode, ExchangeName, ContextKey, Direction, CalcMode
 from ibats_common.strategy import StgBase
 from ibats_common.strategy_handler import strategy_handler_factory
@@ -112,7 +111,7 @@ def _test_use(is_plot):
     logging.info("执行结束 stg_run_id = %d", stg_run_id)
 
     if is_plot:
-        from ibats_common.analysis.plot import show_order, show_cash_and_margin
+        from ibats_common.analysis.plot_db import show_order, show_cash_and_margin, show_rr_with_md
         show_order(stg_run_id, module_name_replacement_if_main='ibats_common.example.ma_cross_stg')
         show_cash_and_margin(stg_run_id)
         show_rr_with_md(stg_run_id, module_name_replacement_if_main='ibats_common.example.ma_cross_stg')
