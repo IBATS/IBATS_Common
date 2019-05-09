0cdc0a3193815d8c186a1ce955eb69225281e237
mmmaaaggg <mmmaaaggg@163.com>
2019-05-09 16:48:11 +0800

    move to tflearn_stg

--- ibats_common/example/tflearn_stg/ai_stg.py
+++ ibats_common/example/tflearn_stg/ai_stg.py
@@ -17,7 +17,7 @@ import numpy as np
 import random
 
 from ibats_common import local_model_folder_path
-from ibats_common.analysis.plot import show_rr_with_md
+from ibats_common.analysis.plot_db import show_rr_with_md
 from ibats_common.common import PeriodType, RunMode, BacktestTradeMode, ExchangeName, ContextKey, Direction, CalcMode
 from ibats_common.strategy import StgBase
 from ibats_common.strategy_handler import strategy_handler_factory
@@ -401,7 +401,7 @@ def _test_use(is_plot):
     logging.info("执行结束 stg_run_id = %d", stg_run_id)
 
     if is_plot:
-        from ibats_common.analysis.plot import show_order, show_cash_and_margin
+        from ibats_common.analysis.plot_db import show_order, show_cash_and_margin
         show_order(stg_run_id, module_name_replacement_if_main='ibats_common.example.ai_stg')
         show_cash_and_margin(stg_run_id)
         show_rr_with_md(stg_run_id, module_name_replacement_if_main='ibats_common.example.ai_stg')
