ce4b15cda7b5f527fbfada822f286c754cd60cb7
mmmaaaggg <mmmaaaggg@163.com>
2019-05-10 17:50:10 +0800

    add get_report_folder_path get_cache_folder_path

--- ibats_common/analysis/__init__.py
+++ ibats_common/analysis/__init__.py
@@ -8,9 +8,31 @@
 @desc    : 
 """
 import pandas as pd
+from functools import lru_cache
 
 pd.set_option('display.width', 240)
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.float_format', '{:,.4f}'.format)
 
+
+@lru_cache(maxsize=1)
+def get_report_folder_path() -> str:
+    import os
+    from ibats_common import module_root_path
+    _report_file_path = os.path.join(module_root_path, 'analysis', 'report')
+    if not os.path.exists(_report_file_path):
+        os.makedirs(_report_file_path)
+
+    return _report_file_path
+
+
+@lru_cache(maxsize=1)
+def get_cache_folder_path() -> str:
+    import os
+    from ibats_common import module_root_path
+    _report_file_path = os.path.join(module_root_path, 'analysis', 'report', "_cache_")
+    if not os.path.exists(_report_file_path):
+        os.makedirs(_report_file_path)
+
+    return _report_file_path
 
