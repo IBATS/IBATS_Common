cfe56204adffeeb09baf6a7e37f5e069e7d4447a
mmmaaaggg <mmmaaaggg@163.com>
2019-05-22 10:18:55 +0800

    add get_trade_date_series

--- ibats_common/example/data/__init__.py
+++ ibats_common/example/data/__init__.py
@@ -9,6 +9,9 @@
 """
 import os
 import pandas as pd
+import numpy as np
+from ibats_utils.mess import str_2_date
+
 from ibats_common import module_root_path
 
 
@@ -16,6 +19,13 @@ def load_data(file_name, encoding=None)-> pd.DataFrame:
     file_path = os.path.join(module_root_path, 'example', 'data', file_name)
     df = pd.read_csv(file_path, encoding=encoding)
     return df
+
+
+def get_trade_date_series():
+    df = load_data('trade_date.csv').astype('datetime64[ns]')
+    # ret_list = [str_2_date(_) for _ in load_data('trade_date.csv').T.to_numpy()[0]]
+    trade_date_s = df.iloc[:, 0]
+    return trade_date_s
 
 
 if __name__ == "__main__":
