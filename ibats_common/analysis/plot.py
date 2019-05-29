86f602833b38615564b0058eef4b35bd8ab9daa8
mmmaaaggg <mmmaaaggg@163.com>
2019-05-29 08:45:40 +0800

    bug fix on plt.savefig and plt.show

--- ibats_common/analysis/plot.py
+++ ibats_common/analysis/plot.py
@@ -70,19 +70,18 @@ def drawdown_plot(df: pd.DataFrame, perf_stats=None, col_name_list=None,
                  for col_name, mdd in col_mdd_dic.items()},
         inplace=True)
 
-    if enable_show_plot:
-        ax = data_df.plot()
-        ax.set_title(f"Drawdown {['{:.2f}%'.format(col_mdd_dic[_] * 100) for _ in col_name_list]}")
-        plt.show()
+    ax = data_df.plot()
+    ax.set_title(f"Drawdown {['{:.2f}%'.format(col_mdd_dic[_] * 100) for _ in col_name_list]}")
 
     if enable_save_plot:
-        ax = data_df.plot()
-        ax.set_title(f"Drawdown {['{:.2f}%'.format(col_mdd_dic[_] * 100) for _ in col_name_list]}")
         file_name = get_file_name(f'drawdown', name=name)
         file_path = os.path.join(get_cache_folder_path(), file_name)
         plt.savefig(file_path, dpi=75)
     else:
         file_path = None
+
+    if enable_show_plot:
+        plt.show()
 
     return data_df, file_path
 
@@ -102,21 +101,18 @@ def plot_corr(df: pd.DataFrame, perf_stats=None,
     if perf_stats is None:
         perf_stats = df.calc_stats()
 
-    def func():
-        ax = perf_stats.plot_correlation()
-        plt.suptitle("Correlation")
-
-    if enable_show_plot:
-        func()
-        plt.show()
+    ax = perf_stats.plot_correlation()
+    plt.suptitle("Correlation")
 
     if enable_save_plot:
-        func()
         file_name = get_file_name(f'correlation', name=name)
         file_path = os.path.join(get_cache_folder_path(), file_name)
         plt.savefig(file_path, dpi=75)
     else:
         file_path = None
+
+    if enable_show_plot:
+        plt.show()
 
     return file_path
 
@@ -136,24 +132,21 @@ def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enab
     else:
         data_df = df
 
-    def func():
-        """绘图函数"""
-        ax = data_df.plot()
-        ax.set_title(
-            f"Return Rate " if name is None else f"Return Rate [{name}] "  
-            f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
-
-    if enable_show_plot:
-        func()
-        plt.show()
+    # """绘图函数"""
+    ax = data_df.plot()
+    ax.set_title(
+        f"Return Rate " if name is None else f"Return Rate [{name}] "  
+        f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
 
     if enable_save_plot:
-        func()
         file_name = get_file_name(f'rr', name=name)
         file_path = os.path.join(get_cache_folder_path(), file_name)
         plt.savefig(file_path, dpi=75)
     else:
         file_path = None
+
+    if enable_show_plot:
+        plt.show()
 
     return file_path
 
@@ -177,24 +170,21 @@ def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, e
     else:
         data_df = df
 
-    def func():
-        """绘图函数"""
-        pd.plotting.scatter_matrix(data_df)
-        plt.suptitle(
-            f"Scatter Matrix " if name is None else f"Scatter Matrix [{name}] "  
-            f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
-
-    if enable_show_plot:
-        func()
-        plt.show()
+    # """绘图函数"""
+    pd.plotting.scatter_matrix(data_df)
+    plt.suptitle(
+        f"Scatter Matrix " if name is None else f"Scatter Matrix [{name}] "  
+        f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
 
     if enable_save_plot:
-        func()
         file_name = get_file_name('scatter_matrix', name=name)
         file_path = os.path.join(get_cache_folder_path(), file_name)
         plt.savefig(file_path, dpi=75)
     else:
         file_path = None
+
+    if enable_show_plot:
+        plt.show()
 
     return file_path
 
@@ -203,26 +193,23 @@ def hist_norm(data, bins=10, enable_show_plot=True, enable_save_plot=False, name
     """hist 分布图及正太分布曲线"""
     n, bins_v, file_name = None, None, None
 
-    def func():
-        """绘图函数"""
-        # ax = pct_change_s.hist(bins=50, density=1)
-        fig, ax = plt.subplots()
-        # the histogram of the data
-        _, _, patches = ax.hist(data, bins, density=True)
-        n, bins_v, mean, std = plot_norm(data, bins=bins, ax=ax)
-        # ax.set_xlabel('pct change')
-        # ax.set_ylabel('change rate')
-        ax.set_title(f"{'Data' if name is None else name} Histogram (mean={mean:.4f} std={std:.4f})")
-
-    if enable_show_plot:
-        func()
-        plt.show()
+    # """绘图函数"""
+    # ax = pct_change_s.hist(bins=50, density=1)
+    fig, ax = plt.subplots()
+    # the histogram of the data
+    _, _, patches = ax.hist(data, bins, density=True)
+    n, bins_v, mean, std = plot_norm(data, bins=bins, ax=ax)
+    # ax.set_xlabel('pct change')
+    # ax.set_ylabel('change rate')
+    ax.set_title(f"{'Data' if name is None else name} Histogram (mean={mean:.4f} std={std:.4f})")
 
     if enable_save_plot:
-        func()
         file_name = get_file_name(f'hist', name=name)
         rr_plot_file_path = os.path.join(get_cache_folder_path(), file_name)
         plt.savefig(rr_plot_file_path, dpi=75)
+
+    if enable_show_plot:
+        plt.show()
 
     return n, bins_v, file_name
 
@@ -347,36 +334,33 @@ def wave_hist(df: pd.DataFrame, columns=None, bins=50, figure_4_each_col=True,
 
     else:
 
-        def func():
-            ax = data_df.hist(bins=bins)
-            if isinstance(ax, np.ndarray):
-                # ax 总是一个 偶数 出现，因此，需要进行一次长度对齐
-                ax_list = list(itertools.chain(*ax))[:len(columns)]
-            else:
-                ax_list = [ax]
+        ax = data_df.hist(bins=bins)
+        if isinstance(ax, np.ndarray):
+            # ax 总是一个 偶数 出现，因此，需要进行一次长度对齐
+            ax_list = list(itertools.chain(*ax))[:len(columns)]
+        else:
+            ax_list = [ax]
 
-            for col_name, ax_sub in zip(columns, ax_list):
-                data = data_df[col_name]
-                try:
-                    data = data[~np.isinf(data)].dropna()
-                except TypeError as exp:
-                    logger.exception('column %s 数据类型无效，无法进行 np.isinf 计算', col_name)
-                    raise exp from exp
+        for col_name, ax_sub in zip(columns, ax_list):
+            data = data_df[col_name]
+            try:
+                data = data[~np.isinf(data)].dropna()
+            except TypeError as exp:
+                logger.exception('column %s 数据类型无效，无法进行 np.isinf 计算', col_name)
+                raise exp from exp
 
-                n, bins_v, mean, std = plot_norm(data, bins=bins, ax=ax_sub)
-                # 字太多无法显示
-                # ax_sub.set_title(f"{col_name}\n(mean={mean:.4f} std={std:.4f})")
-                n_bins_dic[col_name] = (n, bins_v)
-
-        if enable_show_plot:
-            func()
-            plt.show()
+            n, bins_v, mean, std = plot_norm(data, bins=bins, ax=ax_sub)
+            # 字太多无法显示
+            # ax_sub.set_title(f"{col_name}\n(mean={mean:.4f} std={std:.4f})")
+            n_bins_dic[col_name] = (n, bins_v)
 
         if enable_save_plot:
-            func()
             file_name = get_file_name(f'hist', name=name)
             file_path = os.path.join(get_cache_folder_path(), file_name)
             plt.savefig(file_path, dpi=75)
+
+        if enable_show_plot:
+            plt.show()
 
     return n_bins_dic, file_path
 
@@ -391,5 +375,89 @@ def _test_wave_hist():
     n_bins_dic, file_path = wave_hist(df, figure_4_each_col=False, col_transfer_dic=col_transfer_dic)
 
 
+def hist_n_rr(df: pd.DataFrame, n_days, columns=None, bins=50,
+              enable_show_plot=True, enable_save_plot=False, name=None):
+    """
+    波动率分布图
+    :param df:
+    :param n_days: 计算未来 N 日的收益率最高值，最低值
+    :param columns: 显示哪些列
+    :param bins: bar 数量
+    :param enable_show_plot:
+    :param enable_save_plot:
+    :param name:
+    :return:
+    """
+    if columns is not None:
+        data_df = df.loc[:, columns].dropna().copy()
+    else:
+        data_df = df.dropna().copy()
+
+    column_name_list, df_dic, quantile_dic, n_bins_dic, file_path_dic = list(data_df.columns), {}, {}, {}, {}
+    for n_day in n_days:
+        max_df = data_df.rename(
+            columns={_: _ + ' max rr' for _ in column_name_list}
+        ).rolling(n_day).apply(
+            lambda x: max(x/x[0]), raw=True).shift(-(n_day-1))
+        min_df = data_df.rename(
+            columns={_: _ + ' min rr' for _ in column_name_list}
+        ).rolling(n_day).apply(
+            lambda x: min(x/x[0]), raw=True).shift(-(n_day-1))
+        merged_df = max_df.join(min_df).dropna()
+        for col_name in column_name_list:
+            col_names = [col_name + ' max rr', col_name + ' min rr']
+            new_df = merged_df[col_names].copy()
+            df_list = [new_df]
+            idxmax_s = abs(new_df - 1).idxmax(axis=1)
+            # df.loc[idxmax_s != 'close max rr', 'close max rr'] = np.nan
+            # df.loc[idxmax_s != 'close min rr', 'close min rr'] = np.nan
+            label_quantile_dic = {}
+            for label in new_df.columns:
+                data_s = new_df.loc[idxmax_s == label, label].dropna()
+                df_list.append(data_s)
+                label_quantile_dic[label] = data_s.quantile([0.2, 0.33, 0.5, 0.66, 0.8])
+
+            df_dic[(n_day, col_name)] = df_list
+            quantile_dic[(n_day, col_name)] = pd.DataFrame(label_quantile_dic)
+
+    for (n_day, col_name), df_list in df_dic.items():
+
+        hist_kwargs = dict(histtype='stepfilled', alpha=0.8, bins=bins)
+        n_list, bins_v_list, axis_list, labels = [], [], [], []
+        for data_s in df_list[1:]:
+            n, bins_v, axis = plt.hist(data_s.dropna(), **hist_kwargs)
+            labels.append(data_s.name)
+            axis_list.extend(axis)
+
+        plt.legend(axis_list, labels, loc='upper right')
+        if name is None:
+            title = f'hist of max/min {col_name} rr in {n_day} days'
+        else:
+            title = f'{name} hist of max/min {col_name} rr in {n_day} days'
+
+        plt.suptitle(title)
+        n_bins_dic[(n_day, col_name)] = (n_list, bins_v_list)
+
+        if enable_save_plot:
+            file_name = get_file_name(f'hist {n_day} {col_name} future', name=name)
+            file_path = os.path.join(get_cache_folder_path(), file_name)
+            plt.savefig(file_path, dpi=75)
+            file_path_dic[(n_day, col_name)] = file_path
+
+        if enable_show_plot:
+            plt.show()
+
+    ret_dic = dict(df_dic=df_dic, n_bins_dic=n_bins_dic, quantile_dic=quantile_dic)
+    return ret_dic, file_path_dic
+
+
+def _test_hist_n_rr():
+    from ibats_common.example.data import load_data
+    df = load_data('RB.csv').set_index('trade_date').drop('instrument_type', axis=1)
+    df.index = pd.DatetimeIndex(df.index)
+    ret_dic, file_path_dic = hist_n_rr(df, n_days=[3, 5], columns=['close'], enable_save_plot=True)
+
+
 if __name__ == "__main__":
-    _test_wave_hist()
+    # _test_wave_hist()
+    _test_hist_n_rr()
