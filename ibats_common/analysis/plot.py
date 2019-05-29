f076912d61b4903c64eeee3ffebedff6dda4eace
mmmaaaggg <mmmaaaggg@163.com>
2019-05-29 15:04:17 +0800

    add label_distribution

--- ibats_common/analysis/plot.py
+++ ibats_common/analysis/plot.py
@@ -7,17 +7,19 @@
 @contact : mmmaaaggg@163.com
 @desc    : 
 """
-import os
 import itertools
-import pandas as pd
-import matplotlib.pyplot as plt
 import logging
-import seaborn as sns
-from scipy import stats
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
-import ffn
+import pandas as pd
+import seaborn as sns
 from ibats_utils.mess import date_2_str
+from scipy import stats
+
 from ibats_common.analysis import get_cache_folder_path
+from ibats_common.backend.label import calc_label2, calc_label3
 
 logger = logging.getLogger(__name__)
 
@@ -66,7 +68,7 @@ def drawdown_plot(df: pd.DataFrame, perf_stats=None, col_name_list=None,
                        for col_name, s in perf_stats.items() if s.drawdown_details is not None}
     col_mdd_dic = {col_name: mdd for col_name, mdd in data_df.min().items()}
     data_df.rename(
-        columns={col_name: f"{col_name}[{mdd*100:.2f}% {col_mdd_len_dic.setdefault(col_name, '')}]"
+        columns={col_name: f"{col_name}[{mdd * 100:.2f}% {col_mdd_len_dic.setdefault(col_name, '')}]"
                  for col_name, mdd in col_mdd_dic.items()},
         inplace=True)
 
@@ -90,7 +92,7 @@ def drawdown_plot(df: pd.DataFrame, perf_stats=None, col_name_list=None,
 
 
 def plot_corr(df: pd.DataFrame, perf_stats=None,
-                  enable_show_plot=True, enable_save_plot=False, name=None):
+              enable_show_plot=True, enable_save_plot=False, name=None):
     """
     相关性矩阵图
     :param df:
@@ -141,7 +143,7 @@ def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enab
     # """绘图函数"""
     ax = data_df.plot()
     ax.set_title(
-        f"Return Rate " if name is None else f"Return Rate [{name}] "  
+        f"Return Rate " if name is None else f"Return Rate [{name}] "
         f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
 
     if enable_save_plot:
@@ -160,7 +162,8 @@ def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enab
     return file_path
 
 
-def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, enable_show_plot=True, enable_save_plot=False, name=None):
+def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, enable_show_plot=True,
+                        enable_save_plot=False, name=None):
     """
     plot scatter_matrix
     https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.plotting.scatter_matrix.html
@@ -182,7 +185,7 @@ def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, e
     # """绘图函数"""
     pd.plotting.scatter_matrix(data_df)
     plt.suptitle(
-        f"Scatter Matrix " if name is None else f"Scatter Matrix [{name}] "  
+        f"Scatter Matrix " if name is None else f"Scatter Matrix [{name}] "
         f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
 
     if enable_save_plot:
@@ -413,19 +416,22 @@ def hist_n_rr(df: pd.DataFrame, n_days, columns=None, bins=50,
 
     column_name_list, df_dic, quantile_dic, n_bins_dic, file_path_dic = list(data_df.columns), {}, {}, {}, {}
     for n_day in n_days:
+        # 计算各个列未来N日收益率波动
         max_df = data_df.rename(
             columns={_: _ + ' max rr' for _ in column_name_list}
         ).rolling(n_day).apply(
-            lambda x: max(x/x[0]), raw=True).shift(-(n_day-1))
+            lambda x: max(x / x[0]), raw=True).shift(-(n_day - 1))
         min_df = data_df.rename(
             columns={_: _ + ' min rr' for _ in column_name_list}
         ).rolling(n_day).apply(
-            lambda x: min(x/x[0]), raw=True).shift(-(n_day-1))
+            lambda x: min(x / x[0]), raw=True).shift(-(n_day - 1))
+        # 合并数据
         merged_df = max_df.join(min_df).dropna()
         for col_name in column_name_list:
             col_names = [col_name + ' max rr', col_name + ' min rr']
             new_df = merged_df[col_names].copy()
             df_list = [new_df]
+            # 计算收益率分位数信息
             idxmax_s = abs(new_df - 1).idxmax(axis=1)
             # df.loc[idxmax_s != 'close max rr', 'close max rr'] = np.nan
             # df.loc[idxmax_s != 'close min rr', 'close min rr'] = np.nan
@@ -438,8 +444,8 @@ def hist_n_rr(df: pd.DataFrame, n_days, columns=None, bins=50,
             df_dic[(n_day, col_name)] = df_list
             quantile_dic[(n_day, col_name)] = pd.DataFrame(label_quantile_dic)
 
+    # 输出图片
     for (n_day, col_name), df_list in df_dic.items():
-
         hist_kwargs = dict(histtype='stepfilled', alpha=0.8, bins=bins)
         n_list, bins_v_list, axis_list, labels = [], [], [], []
         for data_s in df_list[1:]:
@@ -479,6 +485,80 @@ def _test_hist_n_rr():
     ret_dic, file_path_dic = hist_n_rr(df, n_days=[3, 5], columns=['close'], enable_save_plot=True)
 
 
+def label_distribution(close_df: pd.DataFrame, min_pct: float, max_pct: float, max_future: int,
+                       name=None, **enable_kwargs):
+    """
+    输出分类标签在行情图中的分布情况
+    :param close_df:
+    :param min_pct:
+    :param max_pct:
+    :param max_future:为空则进行2分类，不为空则3分类
+    :param enable_kwargs:
+    :param name:
+    :return:
+    """
+    value_arr = close_df.to_numpy()
+    if max_future is None:
+        target_arr = calc_label2(value_arr, min_pct, max_pct, one_hot=False, dtype='int')
+    else:
+        target_arr = calc_label3(value_arr, min_pct, max_pct, max_future=max_future, one_hot=False, dtype='int')
+
+    ax = close_df.plot()
+    x_values = list(close_df.index)
+    colors = [None, '#d62728', '#2ca02c']
+
+    value_count = len(value_arr)
+    label_list = list(set(target_arr))
+    distribution_rate_df = pd.DataFrame(
+        {_: {'pct': sum(target_arr == _) / value_count} for _ in label_list}, columns=label_list)
+    for label, range_from, range_to in get_range_num_iter(target_arr):
+        color = colors[int(label)]
+        if color is None:
+            continue
+        p = plt.axvspan(x_values[range_from], x_values[range_to], facecolor=color, alpha=0.5)
+
+    file_name = get_file_name(f'label distribution', name=name)
+    file_path = _plot_or_show(file_name=file_name, **enable_kwargs)
+    return distribution_rate_df, file_path
+
+
+def _plot_or_show(enable_save_plot=True, enable_show_plot=True, file_name=None):
+    if enable_save_plot:
+        file_path = os.path.join(get_cache_folder_path(), file_name)
+        plt.savefig(file_path, dpi=75)
+    else:
+        file_path = None
+
+    if enable_show_plot:
+        plt.show()
+
+    plt.cla()
+    plt.clf()
+    return file_path
+
+
+def _test_label_distribution():
+    from ibats_common.example.data import load_data
+    df = load_data('RB.csv').set_index('trade_date').drop('instrument_type', axis=1)
+    df.index = pd.DatetimeIndex(df.index)
+    distribution_rate_df, file_path = label_distribution(df['close'], min_pct=-0.01, max_pct=0.01, max_future=3)
+    logger.info('\n%s', distribution_rate_df)
+    logger.info(file_path)
+
+
+def get_range_num_iter(arr):
+    val_cur, range_from, range_to = arr[0], 0, None
+    for range_to, val in enumerate(arr[1:], start=1):
+        if val != val_cur:
+            yield val_cur, range_from, range_to
+            range_from = range_to
+            val_cur = val
+    else:
+        range_to += 1
+        yield val_cur, range_from, range_to
+
+
 if __name__ == "__main__":
     # _test_wave_hist()
-    _test_hist_n_rr()
+    # _test_hist_n_rr()
+    _test_label_distribution()
