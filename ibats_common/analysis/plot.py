0934ddd3e213f486fe42cd16f8b0afbd257102b8
mmmaaaggg <mmmaaaggg@163.com>
2019-06-16 11:57:40 +0800

    bug fix on plot_or_show

--- ibats_common/analysis/plot.py
+++ ibats_common/analysis/plot.py
@@ -12,7 +12,9 @@ import logging
 import os
 from datetime import datetime
 from functools import lru_cache
-
+import ffn
+import matplotlib
+# matplotlib.use('Qt5Agg')  # windows 下無效
 from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 from pylab import mpl
@@ -28,7 +30,9 @@ from ibats_common.analysis import get_cache_folder_path
 from ibats_common.backend.label import calc_label2, calc_label3
 
 logger = logging.getLogger(__name__)
+logger.debug("matplotlib.get_backend() => %s", matplotlib.get_backend())
 register_matplotlib_converters()
+logger.debug('load %s', ffn)
 
 
 def get_file_name(header, name=None):
@@ -51,7 +55,7 @@ def clean_cache():
 
 
 def drawdown_plot(df: pd.DataFrame, perf_stats=None, col_name_list=None,
-                  enable_show_plot=True, enable_save_plot=False, name=None):
+                  enable_show_plot=True, enable_save_plot=False, name=None, stg_run_id=None):
     """
     回撤曲线
     :param df:
@@ -60,6 +64,7 @@ def drawdown_plot(df: pd.DataFrame, perf_stats=None, col_name_list=None,
     :param enable_show_plot:
     :param enable_save_plot:
     :param name:
+    :param stg_run_id:
     :return:
     """
     if col_name_list is None:
@@ -82,24 +87,15 @@ def drawdown_plot(df: pd.DataFrame, perf_stats=None, col_name_list=None,
     ax = data_df.plot()
     ax.set_title(f"Drawdown {['{:.2f}%'.format(col_mdd_dic[_] * 100) for _ in col_name_list]}")
 
-    if enable_save_plot:
-        file_name = get_file_name(f'drawdown', name=name)
-        file_path = os.path.join(get_cache_folder_path(), file_name)
-        plt.savefig(file_path, dpi=75)
-    else:
-        file_path = None
-
-    if enable_show_plot:
-        plt.show()
-
-    plt.cla()
-    plt.clf()
+    file_name = get_file_name(f'drawdown', name=name)
+    file_path = plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot,
+                             file_name=file_name, stg_run_id=stg_run_id)
 
     return data_df, file_path
 
 
 def plot_corr(df: pd.DataFrame, perf_stats=None,
-              enable_show_plot=True, enable_save_plot=False, name=None):
+              enable_show_plot=True, enable_save_plot=False, name=None, stg_run_id=None):
     """
     相关性矩阵图
     :param df:
@@ -108,6 +104,7 @@ def plot_corr(df: pd.DataFrame, perf_stats=None,
     :param enable_show_plot:
     :param enable_save_plot:
     :param name:
+    :param stg_run_id:
     :return:
     """
     if perf_stats is None:
@@ -116,23 +113,14 @@ def plot_corr(df: pd.DataFrame, perf_stats=None,
     ax = perf_stats.plot_correlation()
     plt.suptitle("Correlation")
 
-    if enable_save_plot:
-        file_name = get_file_name(f'correlation', name=name)
-        file_path = os.path.join(get_cache_folder_path(), file_name)
-        plt.savefig(file_path, dpi=75)
-    else:
-        file_path = None
-
-    if enable_show_plot:
-        plt.show()
-
-    plt.cla()
-    plt.clf()
-
+    file_name = get_file_name(f'correlation', name=name)
+    file_path = plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot,
+                             file_name=file_name, stg_run_id=stg_run_id)
     return file_path
 
 
-def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enable_save_plot=False, name=None):
+def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enable_save_plot=False,
+               name=None, stg_run_id=None):
     """
     Return Rate
     :param df:
@@ -140,6 +128,7 @@ def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enab
     :param enable_show_plot:
     :param enable_save_plot:
     :param name:
+    :param stg_run_id:
     :return:
     """
     if col_name_list is not None:
@@ -151,20 +140,11 @@ def plot_rr_df(df: pd.DataFrame, col_name_list=None, enable_show_plot=True, enab
     ax = data_df.plot(grid=True)
     ax.set_title(
         f"Return Rate " if name is None else f"Return Rate [{name}] "
-        f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
+                                             f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
 
-    if enable_save_plot:
-        file_name = get_file_name(f'rr', name=name)
-        file_path = os.path.join(get_cache_folder_path(), file_name)
-        plt.savefig(file_path, dpi=75)
-    else:
-        file_path = None
-
-    if enable_show_plot:
-        plt.show()
-
-    plt.cla()
-    plt.clf()
+    file_name = get_file_name(f'rr', name=name)
+    file_path = plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot,
+                             file_name=file_name, stg_run_id=stg_run_id)
 
     return file_path
 
@@ -174,12 +154,12 @@ def _test_plot_rr_df():
     df = load_data('RB.csv').set_index('trade_date').drop('instrument_type', axis=1)
     df.index = pd.DatetimeIndex(df.index)
     file_path = plot_rr_df(df, col_name_list=['close'],
-                           enable_show_plot=True, enable_save_plot=True)
+                           enable_show_plot=True, enable_save_plot=True, name='test_use', stg_run_id=2)
     logger.info(file_path)
 
 
 def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, enable_show_plot=True,
-                        enable_save_plot=False, name=None):
+                        enable_save_plot=False, name=None, stg_run_id=None):
     """
     plot scatter_matrix
     https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.plotting.scatter_matrix.html
@@ -191,6 +171,7 @@ def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, e
     :param enable_show_plot:
     :param enable_save_plot:
     :param name:
+    :param stg_run_id:
     :return:
     """
     if col_name_list is not None:
@@ -202,30 +183,25 @@ def plot_scatter_matrix(df: pd.DataFrame, diagonal='hist', col_name_list=None, e
     pd.plotting.scatter_matrix(data_df)
     plt.suptitle(
         f"Scatter Matrix " if name is None else f"Scatter Matrix [{name}] "
-        f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
+                                                f"{date_2_str(min(data_df.index))} - {date_2_str(max(data_df.index))} ({data_df.shape[0]} days)")
 
-    if enable_save_plot:
-        file_name = get_file_name('scatter_matrix', name=name)
-        file_path = os.path.join(get_cache_folder_path(), file_name)
-        plt.savefig(file_path, dpi=75)
-    else:
-        file_path = None
-
-    if enable_show_plot:
-        plt.show()
-
-    plt.cla()
-    plt.clf()
-
+    file_name = get_file_name('scatter_matrix', name=name)
+    file_path = plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot,
+                             file_name=file_name, stg_run_id=stg_run_id)
     return file_path
 
 
-def hist_norm(data, bins=10, enable_show_plot=True, enable_save_plot=False, name=None):
-    """hist 分布图及正太分布曲线"""
-    n, bins_v, file_name = None, None, None
-
-    # """绘图函数"""
-    # ax = pct_change_s.hist(bins=50, density=1)
+def hist_norm(data, bins=10, enable_show_plot=True, enable_save_plot=False, name=None, stg_run_id=None):
+    """
+    hist 分布图及正太分布曲线
+    :param data:
+    :param bins:
+    :param enable_show_plot:
+    :param enable_save_plot:
+    :param name:
+    :param stg_run_id:
+    :return:
+    """
     fig, ax = plt.subplots()
     # the histogram of the data
     _, _, patches = ax.hist(data, bins, density=True)
@@ -235,8 +211,8 @@ def hist_norm(data, bins=10, enable_show_plot=True, enable_save_plot=False, name
     ax.set_title(f"{'Data' if name is None else name} Histogram (mean={mean:.4f} std={std:.4f})")
 
     file_name = get_file_name(f'hist', name=name)
-    rr_plot_file_path = plot_or_show(enable_show_plot=enable_show_plot, enable_save_plot=enable_save_plot, file_name=file_name)
-
+    rr_plot_file_path = plot_or_show(enable_show_plot=enable_show_plot, enable_save_plot=enable_save_plot,
+                                     file_name=file_name, stg_run_id=stg_run_id)
     return n, bins_v, rr_plot_file_path
 
 
@@ -288,7 +264,8 @@ def hist_norm_sns(data, bins=10):
 
 
 def wave_hist(df: pd.DataFrame, columns=None, bins=50, figure_4_each_col=True,
-              col_transfer_dic: (dict, None) = None, enable_show_plot=True, enable_save_plot=False, name=None):
+              col_transfer_dic: (dict, None) = None, enable_show_plot=True, enable_save_plot=False,
+              name=None, stg_run_id=None):
     """
     波动率分布图
     :param df:
@@ -300,6 +277,7 @@ def wave_hist(df: pd.DataFrame, columns=None, bins=50, figure_4_each_col=True,
     :param enable_show_plot:
     :param enable_save_plot:
     :param name:
+    :param stg_run_id:
     :return:
     """
     if columns is not None:
@@ -353,7 +331,8 @@ def wave_hist(df: pd.DataFrame, columns=None, bins=50, figure_4_each_col=True,
                 raise exp from exp
 
             n, bins_v, file_path_sub = hist_norm(
-                data, bins=bins, enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot, name=col_name)
+                data, bins=bins, enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot,
+                name=col_name, stg_run_id=stg_run_id)
             n_bins_dic[col_name] = (n, bins_v)
             if file_path is None:
                 file_path = [file_path_sub]
@@ -383,16 +362,9 @@ def wave_hist(df: pd.DataFrame, columns=None, bins=50, figure_4_each_col=True,
             n_bins_dic[col_name] = (n, bins_v)
 
         plt.grid(True)
-        if enable_save_plot:
-            file_name = get_file_name(f'hist', name=name)
-            file_path = os.path.join(get_cache_folder_path(), file_name)
-            plt.savefig(file_path, dpi=75)
-
-        if enable_show_plot:
-            plt.show()
-
-        plt.cla()
-        plt.clf()
+        file_name = get_file_name(f'hist', name=name)
+        file_path = plot_or_show(enable_show_plot=enable_show_plot, enable_save_plot=enable_save_plot,
+                                 file_name=file_name, stg_run_id=stg_run_id)
 
     return n_bins_dic, file_path
 
@@ -404,11 +376,12 @@ def _test_wave_hist():
     col_transfer_dic = {
         'return': ['open', 'high', 'low', 'close', 'volume']
     }
-    n_bins_dic, file_path = wave_hist(df, figure_4_each_col=False, col_transfer_dic=col_transfer_dic)
+    n_bins_dic, file_path = wave_hist(df, figure_4_each_col=False, col_transfer_dic=col_transfer_dic,
+                                      enable_save_plot=True, enable_show_plot=True, name="test_use", stg_run_id=1)
 
 
 def hist_n_rr(df: pd.DataFrame, n_days, columns=None, bins=50,
-              enable_show_plot=True, enable_save_plot=False, name=None):
+              enable_show_plot=True, enable_save_plot=False, name=None, stg_run_id=None):
     """
     未来N日收益率分布图
     :param df:
@@ -474,17 +447,11 @@ def hist_n_rr(df: pd.DataFrame, n_days, columns=None, bins=50,
         plt.suptitle(title)
         n_bins_dic[(n_day, col_name)] = (n_list, bins_v_list)
 
-        if enable_save_plot:
-            file_name = get_file_name(f'hist {n_day} {col_name} future', name=name)
-            file_path = os.path.join(get_cache_folder_path(), file_name)
-            plt.savefig(file_path, dpi=75)
+        file_name = get_file_name(f'hist {n_day} {col_name} future', name=name)
+        file_path = plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot,
+                                 file_name=file_name, stg_run_id=stg_run_id)
+        if file_path is not None:
             file_path_dic[(n_day, col_name)] = file_path
-
-        if enable_show_plot:
-            plt.show()
-
-        plt.cla()
-        plt.clf()
 
     ret_dic = dict(df_dic=df_dic, n_bins_dic=n_bins_dic, quantile_dic=quantile_dic)
     return ret_dic, file_path_dic
@@ -494,7 +461,7 @@ def _test_hist_n_rr():
     from ibats_common.example.data import load_data
     df = load_data('RB.csv').set_index('trade_date').drop('instrument_type', axis=1)
     df.index = pd.DatetimeIndex(df.index)
-    ret_dic, file_path_dic = hist_n_rr(df, n_days=[3, 5], columns=['close'], enable_save_plot=True)
+    ret_dic, file_path_dic = hist_n_rr(df, n_days=[3, 5], columns=['close'], enable_save_plot=True, stg_run_id=3)
     for k, v in ret_dic['quantile_dic'].items():
         logger.info('%s -> \n%s', k, v)
 
@@ -538,17 +505,25 @@ def label_distribution(close_df: pd.DataFrame, min_rr: float, max_rr: float, max
     return distribution_rate_df, file_path
 
 
-def plot_or_show(enable_save_plot=True, enable_show_plot=True, file_name=None):
+def plot_or_show(enable_save_plot=True, enable_show_plot=True, file_name=None, stg_run_id=None):
     if enable_save_plot:
-        file_path = os.path.join(get_cache_folder_path(), file_name)
+        if stg_run_id is None:
+            folder_path = get_cache_folder_path()
+        else:
+            folder_path = os.path.join(get_cache_folder_path(), str(stg_run_id))
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        file_path = os.path.join(folder_path, file_name)
         plt.savefig(file_path, dpi=75)
     else:
         file_path = None
 
     if enable_show_plot:
         plt.show()
-        # plt.cla()
-        # plt.clf()
+        plt.cla()
+        plt.clf()
 
     return file_path
 
@@ -557,7 +532,8 @@ def _test_label_distribution():
     from ibats_common.example.data import load_data
     df = load_data('RB.csv').set_index('trade_date').drop('instrument_type', axis=1)
     df.index = pd.DatetimeIndex(df.index)
-    distribution_rate_df, file_path = label_distribution(df['close'], min_rr=-0.01, max_rr=0.01, max_future=3)
+    distribution_rate_df, file_path = label_distribution(df['close'], min_rr=-0.01, max_rr=0.01, max_future=3,
+                                                         enable_save_plot=True, enable_show_plot=True, stg_run_id=4)
     logger.info('\n%s', distribution_rate_df)
     logger.info(file_path)
 
@@ -578,7 +554,7 @@ def _test_n_days_rr_distribution():
     from ibats_common.example.data import load_data
     df = load_data('RB.csv').set_index('trade_date').drop('instrument_type', axis=1)
     df.index = pd.DatetimeIndex(df.index)
-    ret_dic, file_path_dic, enable_kwargs = {}, {}, dict(enable_save_plot=True, enable_show_plot=True)
+    ret_dic, file_path_dic, enable_kwargs = {}, {}, dict(enable_save_plot=True, enable_show_plot=True, stg_run_id=5)
     tmp_dic, file_path = hist_n_rr(df, n_days=[3, 5], columns=['close'], **enable_kwargs)
     ret_dic['hist_future_n_rr'] = tmp_dic
     file_path_dic['hist_future_n_rr'] = file_path
@@ -601,7 +577,8 @@ def _test_n_days_rr_distribution():
 
 
 def plot_accuracy(accuracy_df, close_df, split_point_list=None, ax=None,
-                  enable_save_plot=True, enable_show_plot=True, name=None, base_line_list: (None, dict) = None):
+                  enable_save_plot=True, enable_show_plot=True, name=None, base_line_list: (None, dict) = None,
+                  stg_run_id=None):
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -645,7 +622,8 @@ def plot_accuracy(accuracy_df, close_df, split_point_list=None, ax=None,
 
     datetime_str = datetime.now().strftime('%Y-%m-%d_%H_%M_%S_%f')
     file_name = f"{datetime_str}" if name is None else f"{name}_{datetime_str}"
-    plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot, file_name=file_name)
+    plot_or_show(enable_save_plot=enable_save_plot, enable_show_plot=enable_show_plot, file_name=file_name,
+                 stg_run_id=stg_run_id)
 
 
 @lru_cache()
@@ -682,11 +660,11 @@ def _test_plot_accuracy(single_figure=True):
                       base_line_list=base_line_list, enable_save_plot=False, enable_show_plot=False)
         ax = fig.add_subplot(212)
         plot_accuracy(accuracy_df, close_df, ax=ax, name='测试使用2', split_point_list=split_point_list,
-                      base_line_list=base_line_list, enable_save_plot=False, enable_show_plot=False)
-        plot_or_show(enable_show_plot=True, enable_save_plot=True, file_name='测试使用all in one')
+                      base_line_list=base_line_list, enable_save_plot=False, enable_show_plot=False, stg_run_id=6)
+        plot_or_show(enable_show_plot=True, enable_save_plot=True, file_name='测试使用all in one', stg_run_id=6)
     else:
         plot_accuracy(accuracy_df, close_df, name='测试使用2', split_point_list=split_point_list,
-                      base_line_list=base_line_list)
+                      base_line_list=base_line_list, stg_run_id=6)
 
 
 if __name__ == "__main__":
