3ba44d36dd9a0ad437e964557d1b4c5e53e13a93
mmmaaaggg <mmmaaaggg@163.com>
2019-06-18 18:06:04 +0800

    add rl examples

--- ibats_common/example/reinforcement_learning/q_learn.py
+++ ibats_common/example/reinforcement_learning/q_learn.py
@@ -7,8 +7,26 @@
 @contact : mmmaaaggg@163.com
 @desc    : Q Learning Table and Runner
 """
+
+import logging
+
 import numpy as np
 import pandas as pd
+from ibats_utils.mess import date_2_str
+
+logger = logging.getLogger(__name__)
+
+
+def singleton(cls, *args, **kw):
+    instance = {}
+
+    def _singleton():
+        logger.info("单例 %s", cls)
+        if cls not in instance:
+            instance[cls] = cls(*args, **kw)
+        return instance[cls]
+
+    return _singleton
 
 
 class QLearningTable:
@@ -46,50 +64,201 @@ class QLearningTable:
             # append new state to q table
             self.q_table = self.q_table.append(
                 pd.Series(
-                    [0]*len(self.actions),
+                    [0] * len(self.actions),
                     index=self.q_table.columns,
                     name=state,
                 )
             )
 
+    def _get_file_path(self, key):
+        import os
+        from ibats_common import module_root_path
+        # ibats_common/example/module_data
+        folder_path = os.path.join(module_root_path, 'example', 'module_data')
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        file_name = f"q_table_{key}_{len(self.actions)}.csv"
+        file_path = os.path.join(folder_path, file_name)
+        return file_path
 
-def q_learning_runner():
+    def save(self, key):
+        file_path = self._get_file_path(key)
+        self.q_table.to_csv(file_path)
 
-    def update():
-        for episode in range(100):
-            # initial observation
-            observation = env.reset()
+    def load(self, key):
+        file_path = self._get_file_path(key)
+        self.q_table = pd.read_csv(file_path)
 
-            while True:
-                # fresh env
-                env.render()
+    def reset(self):
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
 
-                # RL choose action based on observation
-                action = RL.choose_action(str(observation))
 
-                # RL take action and get next observation and reward
-                observation_, reward, done = env.step(action)
+@singleton
+class RLHandler:
+    __instance = None
 
-                # RL learn from this transition
-                RL.learn(str(observation), action, reward, str(observation_))
+    def __init__(self):
+        action_space = ['empty', 'hold_long', 'hold_short']
+        self.q_table = QLearningTable(actions=list(range(len(action_space))))
+        self.last_state = None
+        self.last_action = None
 
-                # swap observation
-                observation = observation_
+    def init_state(self):
+        # 每一次新 episode 需要重置 state, action
+        self.last_state = None
+        self.last_action = None
 
-                # break while loop when end of this episode
-                if done:
-                    break
+    def get_state_reward(self, md_df: pd.DataFrame):
+        close_s = md_df['close']
+        close_log_arr = np.log(close_s.iloc[-6:])
+        close_latest = close_log_arr.iloc[-1]
+        state = int((close_latest - close_log_arr.iloc[0]) * 1000)
+        if self.last_action is not None:
+            if self.last_action == 0:
+                reward = 0
+            elif self.last_action == 1:
+                reward = int((close_latest - close_log_arr.iloc[-2]) * 1000)
+            elif self.last_action == 2:
+                reward = - int((close_latest - close_log_arr.iloc[-2]) * 1000)
+            else:
+                raise ValueError('last_action = %d 值不合法', self.last_action)
+        else:
+            reward = 0
 
-        # end of game
-        print('game over')
-        env.destroy()
+        return state, reward
 
-    env = Maze()
-    RL = QLearningTable(actions=list(range(env.n_actions)))
+    def choose_action(self, state):
+        self.last_action = self.q_table.choose_action(state)
+        self.last_state = state
+        return self.last_action
 
-    env.after(100, update)
-    env.mainloop()
+    def learn(self, r, s_):
+        if self.last_state is None:
+            return
+        self.q_table.learn(self.last_state, self.last_action, r, s_)
+
+
+class Env:
+    def __init__(self, date_from, date_to):
+        self.action_space = ['empty', 'hold_long', 'hold_short']
+        self.n_actions = len(self.action_space)
+        self.stg_handler = None
+        self.train_date_from, self.train_date_to = date_2_str(date_from), date_2_str(date_to)
+        self.rl_handler = RLHandler()
+        self._build()
+
+    def _build(self):
+        from ibats_common import module_root_path
+        import os
+        from ibats_common.common import RunMode
+        from ibats_common.common import CalcMode
+        from ibats_common.common import PeriodType
+        from ibats_common.common import BacktestTradeMode
+        from ibats_common.strategy_handler import strategy_handler_factory
+        from ibats_common.common import ExchangeName
+        from ibats_common.example.reinforcement_learning.rl_stg import RLStg
+        # 参数设置
+        instrument_type = 'RB'
+        run_mode = RunMode.Backtest_FixPercent
+        calc_mode = CalcMode.Normal
+        strategy_params = {'unit': 1,
+                           'module_name': 'ibats_common.example.reinforcement_learning.q_learn',
+                           'class_name': 'RLHandler',
+                           'for_train': True}
+        md_agent_params_list = [{
+            'md_period': PeriodType.Min1,
+            'instrument_id_list': [instrument_type],
+            'datetime_key': 'trade_date',
+            'init_md_date_from': '1995-1-1',  # 行情初始化加载历史数据，供策略分析预加载使用
+            'init_md_date_to': self.train_date_from,
+            # 'C:\GitHub\IBATS_Common\ibats_common\example\ru_price2.csv'
+            'file_path': os.path.abspath(os.path.join(module_root_path, 'example', 'data', 'RB.csv')),
+            'symbol_key': 'instrument_type',
+        }]
+        if run_mode == RunMode.Realtime:
+            trade_agent_params = {
+            }
+            strategy_handler_param = {
+            }
+        elif run_mode == RunMode.Backtest:
+            trade_agent_params = {
+                'trade_mode': BacktestTradeMode.Order_2_Deal,
+                'init_cash': 1000000,
+                "calc_mode": calc_mode,
+            }
+            strategy_handler_param = {
+                'date_from': self.train_date_from,  # 策略回测历史数据，回测指定时间段的历史行情
+                'date_to': self.train_date_to,
+            }
+        else:
+            # RunMode.Backtest_FixPercent
+            trade_agent_params = {
+                'trade_mode': BacktestTradeMode.Order_2_Deal,
+                "calc_mode": calc_mode,
+            }
+            strategy_handler_param = {
+                'date_from': self.train_date_from,  # 策略回测历史数据，回测指定时间段的历史行情
+                'date_to': self.train_date_to,
+            }
+
+        # 初始化策略处理器
+        self.stg_handler = stg_handler = strategy_handler_factory(
+            stg_class=RLStg,
+            strategy_params=strategy_params,
+            md_agent_params_list=md_agent_params_list,
+            exchange_name=ExchangeName.LocalFile,
+            run_mode=run_mode,
+            trade_agent_params=trade_agent_params,
+            strategy_handler_param=strategy_handler_param,
+        )
+        stg_run_id = stg_handler.stg_run_id
+        stg_handler.start()
+        logging.info("执行开始 stg_run_id = %d", stg_run_id)
+        # time.sleep(10)
+        # stg_handler.keep_running = False
+        # stg_handler.join()
+        # logging.info("执行结束 stg_run_id = %d", stg_run_id)
+        # if is_plot:
+        #     from ibats_common.analysis.summary import summary_stg_2_docx
+        #     from ibats_utils.mess import open_file_with_system_app
+        #     file_path = summary_stg_2_docx(stg_run_id, enable_clean_cache=False)
+        #     if file_path is not None:
+        #         open_file_with_system_app(file_path)
+
+    def set_stop(self):
+        if self.stg_handler is not None:
+            stg_run_id = self.stg_handler.stg_run_id
+            self.stg_handler.keep_running = False
+            logger.info('结束旧任务 stg_run_id=%d', stg_run_id)
+            self.stg_handler.join()
+            logger.info('结束旧任务 stg_run_id=%d 完成', stg_run_id)
+            # 输出绩效报告
+            # from ibats_common.analysis.summary import summary_stg_2_docx
+            # from ibats_utils.mess import open_file_with_system_app
+            # file_path = summary_stg_2_docx(stg_run_id, enable_clean_cache=False)
+            # if file_path is not None:
+            #     open_file_with_system_app(file_path)
+
+    def reset(self):
+        self.set_stop()
+        self._build()
+
+    def destroy(self):
+        self.set_stop()
+
+
+def main(trade_date_from='2010-1-1', trade_date_to='2018-10-18'):
+    env = Env(trade_date_from, trade_date_to)
+    rl_handler = RLHandler()
+    for episode in range(2):
+        # fresh env
+        env.reset()
+        logger.info("\n%s", rl_handler.q_table.q_table)
+
+    # end of game
+    env.destroy()
+    logger.info('RL Over')
 
 
 if __name__ == "__main__":
-    pass
+    main()
---
+++ ibats_common/example/reinforcement_learning/rl_stg.py
@@ -0,0 +1,212 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/11/7 10:42
+@File    : ma_cross_stg.py
+@contact : mmmaaaggg@163.com
+@desc    : 简单的 MA5、MA10金叉、死叉多空策略，仅供测试及演示使用（回测模式：固定仓位比例）
+"""
+from ibats_utils.mess import load_class
+
+from ibats_common.common import BacktestTradeMode, ContextKey, Direction, CalcMode
+from ibats_common.strategy import StgBase
+from ibats_common.strategy_handler import strategy_handler_factory
+from ibats_local_trader.agent.md_agent import *
+from ibats_local_trader.agent.td_agent import *
+
+logger = logging.getLogger(__name__)
+
+
+class RLStg(StgBase):
+
+    def __init__(self, module_name, class_name, for_train, unit=1):
+        super().__init__()
+        self.unit = unit
+        self.state = None
+        self.module_name, self.class_name = module_name, class_name
+        self.for_train = for_train      # True 代表当期实例供RL训练使用，否则，用于策略模拟使用
+        self.rl_handler_class = load_class(module_name, class_name)
+        self.rl_handler = self.rl_handler_class()
+        self.train_date_latest = None
+        self.retrain_period = pd.Timedelta(days=10)
+        self.train_date_from = None
+
+    def on_prepare_min1(self, md_df, context):
+        self.rl_handler.init_state()
+        if not self.for_train:
+            # for_train == False 当期为策略运行使用，在 on_prepare 阶段以及 on_period 定期进行重新训练
+            trade_date_s = md_df['trade_date']
+            self.train_date_from = trade_date_s.iloc[0] + self.retrain_period
+            trade_date_to = trade_date_s.iloc[-1]
+            train(self.rl_handler_class, self.train_date_from, trade_date_to, episode_count=3)
+            self.train_date_latest = trade_date_to
+
+        state, _ = self.rl_handler.get_state_reward(md_df)
+        _ = self.rl_handler.choose_action(state)
+
+    def on_min1(self, md_df, context):
+        trade_date_s = md_df['trade_date']
+        trade_date_to = trade_date_s.iloc[-1]
+        if not self.for_train and trade_date_to > (self.train_date_latest + self.retrain_period):
+            # for_train == False 当期为策略运行使用，在 on_prepare 阶段以及 on_period 定期进行重新训练
+            train(self.rl_handler_class, self.train_date_from, trade_date_to, episode_count=3)
+
+        close = md_df['close'].iloc[-1]
+        state, reward = self.rl_handler.get_state_reward(md_df)
+        self.rl_handler.learn(reward, state)
+        action = self.rl_handler.choose_action(state)
+
+        instrument_id = context[ContextKey.instrument_id_list][0]
+        if action == 1:
+            # 做多
+            position_date_pos_info_dic = self.get_position(instrument_id)
+            no_holding_target_position = True
+            if position_date_pos_info_dic is not None:
+                for position_date, pos_info in position_date_pos_info_dic.items():
+                    if pos_info.position == 0:
+                        continue
+                    direction = pos_info.direction
+                    if direction == Direction.Short:
+                        self.close_short(instrument_id, close, pos_info.position)
+                    elif direction == Direction.Long:
+                        no_holding_target_position = False
+            if no_holding_target_position:
+                self.open_long(instrument_id, close, self.unit)
+        elif action == 2:
+            # 做空
+            position_date_pos_info_dic = self.get_position(instrument_id)
+            no_holding_target_position = True
+            if position_date_pos_info_dic is not None:
+                for position_date, pos_info in position_date_pos_info_dic.items():
+                    if pos_info.position == 0:
+                        continue
+                    direction = pos_info.direction
+                    if direction == Direction.Long:
+                        self.close_long(instrument_id, close, pos_info.position)
+                    elif direction == Direction.Short:
+                        no_holding_target_position = False
+            if no_holding_target_position:
+                self.open_short(instrument_id, close, self.unit)
+        elif action == 0:
+            # 空仓
+            position_date_pos_info_dic = self.get_position(instrument_id)
+            if position_date_pos_info_dic is not None:
+                for position_date, pos_info in position_date_pos_info_dic.items():
+                    if pos_info.position == 0:
+                        continue
+                    direction = pos_info.direction
+                    if direction == Direction.Long:
+                        self.close_long(instrument_id, close, pos_info.position)
+                    elif direction == Direction.Short:
+                        self.close_short(instrument_id, close, pos_info.position)
+
+
+def train(rl_handler_class, trade_date_from, trade_date_to, episode_count):
+    """
+    具体功能参见 ibats_common.example.reinforcement_learning.q_learn import main
+    :param rl_handler_class:
+    :param trade_date_from:
+    :param trade_date_to:
+    :param episode_count:
+    :return:
+    """
+    from ibats_common.example.reinforcement_learning.q_learn import Env
+    logger.info('开始训练：[%s, %s]', trade_date_from, trade_date_to)
+    env = Env(trade_date_from, trade_date_to)
+    rl_handler = rl_handler_class()
+    for episode in range(episode_count):
+        # fresh env
+        env.reset()
+        logger.info("\n%s", rl_handler.q_table.q_table)
+
+    # end of game
+    env.destroy()
+    logger.info('RL Over')
+
+
+def _test_use(is_plot):
+    from ibats_common import module_root_path
+    import os
+    # 参数设置
+    instrument_type = 'RB'
+    run_mode = RunMode.Backtest_FixPercent
+    calc_mode = CalcMode.Normal
+    strategy_params = {'unit': 1,
+                       'module_name': 'ibats_common.example.reinforcement_learning.q_learn',
+                       'class_name': 'RLHandler',
+                       'for_train': False}
+    md_agent_params_list = [{
+        'md_period': PeriodType.Min1,
+        'instrument_id_list': [instrument_type],
+        'datetime_key': 'trade_date',
+        'init_md_date_from': '1995-1-1',  # 行情初始化加载历史数据，供策略分析预加载使用
+        'init_md_date_to': '2010-1-1',
+        # 'C:\GitHub\IBATS_Common\ibats_common\example\ru_price2.csv'
+        'file_path': os.path.abspath(os.path.join(module_root_path, 'example', 'data', 'RB.csv')),
+        'symbol_key': 'instrument_type',
+    }]
+    if run_mode == RunMode.Realtime:
+        trade_agent_params = {
+        }
+        strategy_handler_param = {
+        }
+    elif run_mode == RunMode.Backtest:
+        trade_agent_params = {
+            'trade_mode': BacktestTradeMode.Order_2_Deal,
+            'init_cash': 1000000,
+            "calc_mode": calc_mode,
+        }
+        strategy_handler_param = {
+            'date_from': '2010-1-1',  # 策略回测历史数据，回测指定时间段的历史行情
+            'date_to': '2018-10-18',
+        }
+    else:
+        # RunMode.Backtest_FixPercent
+        trade_agent_params = {
+            'trade_mode': BacktestTradeMode.Order_2_Deal,
+            "calc_mode": calc_mode,
+        }
+        strategy_handler_param = {
+            'date_from': '2010-1-1',  # 策略回测历史数据，回测指定时间段的历史行情
+            'date_to': '2018-10-18',
+        }
+
+    # 初始化策略处理器
+    stghandler = strategy_handler_factory(
+        stg_class=RLStg,
+        strategy_params=strategy_params,
+        md_agent_params_list=md_agent_params_list,
+        exchange_name=ExchangeName.LocalFile,
+        run_mode=run_mode,
+        trade_agent_params=trade_agent_params,
+        strategy_handler_param=strategy_handler_param,
+    )
+    stghandler.start()
+    time.sleep(10)
+    stghandler.keep_running = False
+    stghandler.join()
+    stg_run_id = stghandler.stg_run_id
+    logging.info("执行结束 stg_run_id = %d", stg_run_id)
+
+    if is_plot:
+        # from ibats_common.analysis.plot_db import show_order, show_cash_and_margin, show_rr_with_md
+        # from ibats_common.analysis.summary import summary_rr
+        # show_order(stg_run_id)
+        # df = show_cash_and_margin(stg_run_id)
+        # sum_df, symbol_rr_dic, save_file_path_dic = show_rr_with_md(stg_run_id)
+        # for symbol, rr_df in symbol_rr_dic.items():
+        #     col_transfer_dic = {'return': rr_df.columns}
+        #     summary_rr(rr_df, figure_4_each_col=True, col_transfer_dic=col_transfer_dic)
+        from ibats_common.analysis.summary import summary_stg_2_docx
+        from ibats_utils.mess import open_file_with_system_app
+        file_path = summary_stg_2_docx(stg_run_id)
+        if file_path is not None:
+            open_file_with_system_app(file_path)
+
+    return stg_run_id
+
+
+if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG, format=config.LOG_FORMAT)
+    _test_use(is_plot=True)
