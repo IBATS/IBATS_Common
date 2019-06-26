46863c0b4b430fa458a5cd86170b8fc345eec299
mmmaaaggg <mmmaaaggg@163.com>
2019-06-26 22:59:21 +0800

    bug fix on show_accuracy

--- ibats_common/example/tf_stg/lstm3_stg.py
+++ ibats_common/example/tf_stg/lstm3_stg.py
@@ -36,7 +36,6 @@ pip3 install tensorflow sklearn tflearn
 
 """
 import os
-import random
 import re
 from collections import defaultdict
 from datetime import datetime, timedelta
@@ -46,9 +45,9 @@ import tensorflow as tf
 import tflearn
 from ibats_utils.mess import date_2_str, get_last
 from sklearn.model_selection import train_test_split
-from tflearn import conv_2d, max_pool_2d, local_response_normalization, fully_connected, dropout
 
 from ibats_common import module_root_path
+from ibats_common.analysis.plot import show_accuracy
 from ibats_common.backend.factor import get_factor
 from ibats_common.backend.label import calc_label3
 from ibats_common.common import BacktestTradeMode, ContextKey, Direction, CalcMode
@@ -95,9 +94,9 @@ class AIStg(StgBase):
         # enable_load_model_if_exist 将会在调用 self.load_model_if_exist 时进行检查
         # 如果该字段为 False，调用 load_model_if_exist 时依然可以传入参数的方式加载已有模型
         # 该字段与 self.load_model_if_exist 函数的 enable_load_model_if_exist参数是 “or” 的关系
-        self.enable_load_model_if_exist = False
+        self.enable_load_model_if_exist = True
         if self.enable_load_model_if_exist:
-            self.base_folder_path = folder_path = os.path.join(module_root_path, f'tf_saves_2019-06-16_09_33_30')
+            self.base_folder_path = folder_path = os.path.join(module_root_path, 'tf_saves_2019-06-26_07_28_11')
         else:
             datetime_str = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
             self.base_folder_path = folder_path = os.path.join(module_root_path, f'tf_saves_{datetime_str}')
@@ -701,50 +700,6 @@ class AIStg(StgBase):
         return date_file_path_pair_list
 
 
-def show_accuracy(real_ys, pred_ys, close_df: pd.DataFrame, split_point_list=None,
-                  base_line_list: (None, dict) = None):
-    trade_date_index = close_df.index
-    date_from_str, date_to_str = date_2_str(trade_date_index[0]), date_2_str(trade_date_index[-1])
-    # 检查长度是否一致
-    if len(real_ys) != len(pred_ys) or len(real_ys) == 0:
-        logger.error("[%s - %s] len(real_ys)=%d, len(pred_ys)=%d 不一致",
-                     date_from_str, date_to_str, len(real_ys), len(pred_ys))
-        return
-    # 分析成功率
-    # 累计平均成功率
-    accuracy = sum(pred_ys == real_ys) / len(pred_ys) * 100
-    logger.info("模型准确率 [%s - %s] accuracy: %.2f%%", date_from_str, date_to_str, accuracy)
-    is_fit_arr = pred_ys == real_ys
-    accuracy_list, fit_sum = [], 0
-    for tot_count, (is_fit, trade_date) in enumerate(zip(is_fit_arr, trade_date_index), start=1):
-        if is_fit:
-            fit_sum += 1
-        accuracy_list.append(fit_sum / tot_count)
-
-    accuracy_df = pd.DataFrame({'accuracy': accuracy_list}, index=trade_date_index)
-    from ibats_common.analysis.plot import plot_accuracy
-    import matplotlib.pyplot as plt
-    fig = plt.figure(figsize=(8, 12))
-    ax = fig.add_subplot(211)
-    plot_accuracy(accuracy_df, close_df, ax=ax, base_line_list=base_line_list,
-                  name=f'Accumulation Avg Accuracy [{date_from_str}{date_to_str}]',
-                  split_point_list=split_point_list, enable_show_plot=False, enable_save_plot=False)
-    # 移动平均成功率
-    accuracy_list, win_size = [], 60
-    for idx in range(win_size, len(is_fit_arr)):
-        accuracy_list.append(sum(is_fit_arr[idx - win_size:idx] / win_size))
-
-    close2_df = close_df.iloc[win_size:]
-    ax2 = fig.add_subplot(212)
-    accuracy_df = pd.DataFrame({'accuracy': accuracy_list}, index=close2_df.index)
-    plot_accuracy(accuracy_df, close2_df, ax=ax2, base_line_list=base_line_list,
-                  name=f'{win_size} Moving Avg Accuracy [{date_from_str}{date_to_str}]',
-                  split_point_list=split_point_list, enable_show_plot=False, enable_save_plot=False)
-    file_name = f"accuracy [{date_from_str}-{date_to_str}]"
-    from ibats_common.analysis.plot import plot_or_show
-    plot_or_show(enable_save_plot=True, enable_show_plot=True, file_name=file_name)
-
-
 def _test_use(is_plot):
     from ibats_common import module_root_path
     import os
@@ -814,23 +769,7 @@ def _test_use(is_plot):
     return stg_run_id
 
 
-def _test_show_accuracy():
-    real_ys, pred_ys = np.random.randint(1, 3, size=100), np.random.randint(1, 3, size=100)
-    date_arr = pd.date_range(pd.to_datetime('2018-01-01'),
-                             pd.to_datetime('2018-01-01') + pd.Timedelta(days=99))
-    date_index = pd.DatetimeIndex(date_arr)
-    close_df = pd.DataFrame({'close': np.sin(np.linspace(0, 10, 100))}, index=date_index)
-
-    split_point_list = np.random.randint(len(date_arr), size=10)
-    split_point_list.sort()
-    split_point_list = date_arr[split_point_list]
-    base_line_list = [0.3, 0.6]
-
-    show_accuracy(real_ys, pred_ys, close_df, split_point_list, base_line_list)
-
-
 if __name__ == '__main__':
     # logging.basicConfig(level=logging.DEBUG, format=config.LOG_FORMAT)
     is_plot = True
     _test_use(is_plot)
-    # _test_show_accuracy()
