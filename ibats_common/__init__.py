59cc306b3d73b8d51cf34d1b77a9047ea0ed8e16
mmmaaaggg <mmmaaaggg@163.com>
2019-08-25 15:01:21 +0800

    exception handle

--- ibats_common/__init__.py
+++ ibats_common/__init__.py
@@ -7,11 +7,11 @@
 @contact : mmmaaaggg@163.com
 @desc    : 
 """
-from ibats_utils.mess import get_folder_path
 import os
 import re
 
+from ibats_utils.mess import get_folder_path
 
 module_root_path = get_folder_path(re.compile(r'^ibats[\w]+'), create_if_not_found=False)  # 'ibats_common'
-root_parent_path = os.path.abspath(
-    os.path.join(module_root_path, os.path.pardir))
+root_parent_path = os.path.abspath(os.path.join(module_root_path, os.path.pardir)) \
+    if module_root_path is not None else None
