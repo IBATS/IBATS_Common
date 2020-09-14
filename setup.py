221465c094ad935524ee42354b6d9f99a6b1ee3c
mmmaaaggg <mmmaaaggg@163.com>
2020-09-14 09:57:31 +0800

    refactor: lower dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding='utf-8') as rm:
     long_description = rm.read()
 
 setup(name='IBATS_Common',
-      version='0.19.1',
+      version='0.19.2',
       description='IBATS（Integration Backtest Analysis Trade System）的公共模块，所有Feeder, Trader均集成这个模块，并使用其提供的一些公共工具',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -41,7 +41,7 @@ setup(name='IBATS_Common',
           'numpy<=1.19.0',
           'pandas>=1.1.2',
           'scipy>=1.4.1',
-          'statsmodels>=0.12.0',
+          'statsmodels>=0.11.1',
           'redis>=2.10.6',
           'SQLAlchemy>=1.3.2',
           'mysqlclient>=1.4.6',
